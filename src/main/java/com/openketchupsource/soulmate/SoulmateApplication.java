71be66f1a8b72cb5c5df5f2e2f9f2a063091ec9d
채린 <166609121+cherrynniii@users.noreply.github.com>
2025-05-11 18:34:15 +0900

    Revert "kakao login"

--- src/main/java/com/openketchupsource/soulmate/SoulmateApplication.java
+++ src/main/java/com/openketchupsource/soulmate/SoulmateApplication.java
@@ -2,12 +2,8 @@ package com.openketchupsource.soulmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "com.openketchupsource.soulmate")
-@EnableJpaAuditing
 public class SoulmateApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/openketchupsource/soulmate/external/oauth/dto/SocialLoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.openketchupsource.soulmate.external.oauth.dto;
-
-public record SocialLoginRequest(
-        String code
-) {
-    public static SocialLoginRequest of(String code) {
-        return new SocialLoginRequest(code);
-    }
-}

fa5871d91ca9a42e6ceae13c1939f3e4ebc6848e
acidify04 <peh4622@naver.com>
2025-05-15 15:20:30 +0900

    feat #16 : Comment, Character entity 생성

---
+++ src/main/java/com/openketchupsource/soulmate/domain/Character.java
@@ -0,0 +1,26 @@
+package com.openketchupsource.soulmate.domain;
+
+import com.openketchupsource.soulmate.member.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Character extends BaseTimeEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String personality;
+}
